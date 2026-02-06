efa0c40e18153c6c15d8e704c9e5ada72494f12b
MCarnegie <marcocarnegie4@gmail.com>
2026-02-06 12:06:02 -0500

    init commis

---
+++ increments/js/game.js
@@ -0,0 +1,13 @@
+numClicks = 0
+
+document.getElementById("cow").addEventListener("click", () =>{
+    numClicks++
+    cowSelect(document.getElementById("cow").classList.add)
+})
+
+
+
+function cowSelect(img){
+    img.add
+}
+    
