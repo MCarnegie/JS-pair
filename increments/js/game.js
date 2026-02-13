d096ebd7853bb540d08b2737d2325715c8f15463
MCarnegie <marcocarnegie4@gmail.com>
2026-02-13 11:47:39 -0500

    adding upgrades in progress

--- increments/js/game.js
+++ increments/js/game.js
@@ -1,8 +1,32 @@
-numClicks = 0
+const player = {
+    numClicks: 0,
+    numClickers: 0,
+    numAutoClickSpeed: 1,
+    numUpgradeClick: 1,
+}
+let moreClickersPrice = 0; 
+let autoClickSpeedPrice = 0;
+let upgradeClick = 0;
 
 document.getElementById("cow").addEventListener("click", () => {
-    numClicks++
+    player.numClicks++
+    document.getElementById("score").innerHTML = player.numClicks
     Select(document.getElementById("cow"))
+})
+
+document.getElementById("moreClicks").addEventListener("click", () =>{
+    player.numClickers++;
+    Select(document.getElementById("moreClicks"))
+})
+
+document.getElementById("autoClickSpeed").addEventListener("click", () =>{
+    player.numAutoClickSpeed++;
+    Select(document.getElementById("autoClickSpeed"))
+})
+
+document.getElementById("upgradeClick").addEventListener("click", () =>{
+    player.numClickers++;
+    Select(document.getElementById("upgradeClick"))
 })
 
 
